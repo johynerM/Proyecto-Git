fe7d253885b2538d51f59f899fec0c4ee7826212
juansebastiansierravelasco <juansebastian13s_@hotmail.com>
2026-09-19 18:02:38 -0500

    feat: :rocket: funciones y registro
    se le agrego una funcion y tambien se le agrego para que pueda guardar el archivo json

--- Registrar_devolución.py
+++ Registrar_devolución.py
@@ -1,11 +1,16 @@
+import json
 def devolucion():
-codigo=input("Ingrese el codigo del producto: ")
-for producto in productos:
+ codigo=input("Ingrese el codigo del producto: ")
+ for producto in inventario:
     if producto["codigo"]==codigo:
         cantidad=int(input("ingrese la cuantos productos desea devolver: "))
         producto["cantidad"]+=cantidad
         print("Devolucion realizado con exito")
+        with open("inventario.json", "w") as archivo:
+            json.dump(inventario, archivo, indent=4)
+        break
     else:
         print("Producto no encontrado")
+
 
             
