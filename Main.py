f7c5c7ab58df317f10266f7bbcce5645be702603
johynerM <johynersmc1029@gmail.com>
2026-09-19 16:36:56 -0500

    style: actualizar menu principal

--- Main.py
+++ Main.py
@@ -1,1 +1,27 @@
 #LINEA PRINCIPAL VISUALIZACION USER
+
+print("========================================")
+print("Bienvenido a la Biblioteca Comunitaria Horizonte ")
+while True:
+    opcion = int(input("Ingresa la opción que deseas ejecutar: "))
+    print("1. Registrar ítem\n2.Listar ítems\n3. Buscar ítem\n4. Registrar préstamo\n5. Registrar Devolucion\n6.Salir")
+    if opcion == 1:
+        from Registrar_item import registro
+        registro()
+    elif opcion == 2:
+        from Listar_item import listar
+        listar()
+    elif opcion == 3:
+        from Buscar_item import buscar
+        buscar()
+    elif opcion == 4:
+        from Registar_prestamo import prestamo
+        prestamo()
+    elif opcion == 5:
+        from Registrar_devolucion import devolucion
+        devolucion()
+    elif opcion==6:
+        print("Buen dia, gracias por visitarnos😁")
+        break
+    
+    
